c08d591d39cc37acf7465f123d3d1bab7c2d189d
jtiguila <67078001+jtiguila@users.noreply.github.com>
2020-06-23 00:36:33 -0700

    Word Count Added
    Adding function to count most used word in a text file. Added text file that contains pasrt of the script from Lord of the Rings

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
 if __name__ == "__main__":
-    #any excutable code
+
+    #Prints out 'Josue Tiguila Jr.'
     name = 'Josue Tiguila Jr.'
     print(f'Hello {name}')
+
+    #Counts most used word
+    with open('01_random.txt') as reader: #scrpit from Lord of the Rings
+        counts = dict()
+        for line in reader:
+            words = line.split()
+            for word in words:
+                counts[word] = counts.get(word, 0) + 1
+                
+    # Get the max value based on the key's value
+    most_used = max(counts, key=lambda key: counts[key])
+    print(f"The most used word is '{most_used}' appearing {counts[most_used]} times.")
