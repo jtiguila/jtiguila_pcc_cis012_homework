b040f332135afcad12b83290dc091cf52c59b16f
jtiguila <67078001+jtiguila@users.noreply.github.com>
2020-06-25 16:15:05 -0700

    Create temp.py
    Program that asks Celsius and converts to Fahrenheit

---
+++ temp.py
@@ -0,0 +1,14 @@
+# Temp.py
+# Module 2 Homework - CIS 12
+# Instructor James Mertz
+# By Josue Tiguila
+
+#Prompt user for Celsius Temperature
+print ("Please enter a Temeperature in Celsius")
+celsius = float(input())
+
+#Converts to Fahrenheit
+fahrenheit = celsius * 1.8 + 32
+
+#Prints out Fahrenheit Temp
+print("Here is the temperature in Fahrenheit: " + str(fahrenheit))
